36055e7592f012798b5dbb3d1193c85de8b30c22
Adi Goldner <96goldner@gmail.com>
2026-05-21 15:03:03 +0300

    eslinter has a problem with vue extesion so it ignores now one line in client files

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../stores/auth";
 
 export const api = axios.create({
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	baseURL: import.meta.env.VITE_API_URL,
 });
 
